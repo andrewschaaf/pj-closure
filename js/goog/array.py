720893e5795c0d95c070e0d026592a827d1daf4a
Andrew Schaaf <andrew@andrewschaaf.com>
2010-11-08 10:12:13 -0500

    goog.array...

--- js/goog/array.py
+++ js/goog/array.py
@@ -21,6 +21,9 @@ from goog import bind, isString
 ARRAY_PROTOTYPE_ = Array.prototype
 
 
+def concat(var_args):
+  return ARRAY_PROTOTYPE_.concat.apply(ARRAY_PROTOTYPE_, arguments)
+
 
 def forEach(arr, f, opt_obj):
   #DIFF: goog runs this if-statement at load-time
@@ -69,4 +72,75 @@ def reduce(arr, f, val, opt_obj):
   forEach(arr, f)
   
   return rval
+
+
+def slice(arr, start, opt_end):
+  #goog.asserts.assert(arr.length != null);
+  
+  # passing 1 arg to slice is not the same as passing 2 where the second is
+  # null or undefined (in that case the second argument is treated as 0).
+  # we could use slice on the arguments object and then use apply instead of
+  # testing the length
+  if arguments.length <= 2:
+    return ARRAY_PROTOTYPE_.slice.call(arr, start)
+  else:
+    return ARRAY_PROTOTYPE_.slice.call(arr, start, opt_end)
+
+
+def splice(arr, index, howMany, var_args):
+  #goog.asserts.assert(arr.length != null)
+  return ARRAY_PROTOTYPE_.splice.apply(
+      arr, slice(arguments, 1))
+
+
+
+def insertAt(arr, obj, opt_i):
+  splice(arr, opt_i, 0, obj)
+
+
+
+def filter(arr, f, opt_obj):
+  if ARRAY_PROTOTYPE_.filter:
+    #goog.asserts.assert(arr.length != null);
+    return ARRAY_PROTOTYPE_.filter.call(arr, f, opt_obj)
+  else:
+    res = []
+    resLength = 0
+    arr2 = arr.split('') if isString(arr) else arr
+    for i in range(len(arr)):
+      if i in arr2:
+        val = arr2[i]
+        if f.call(opt_obj, val, i, arr):
+          # Is this better than .push?
+          resLength += 1
+          res[resLength] = val
+    return res
+
+
+def indexOf(arr, obj, opt_fromIndex):
+  if ARRAY_PROTOTYPE_.indexOf:
+    #goog.asserts.assert(arr.length != null);
+    return ARRAY_PROTOTYPE_.indexOf.call(arr, obj, opt_fromIndex)
+  else:
+    
+    fromIndex = (
+                  0
+                  if opt_fromIndex == None else
+                  (
+                    Math.max(0, arr.length + opt_fromIndex)
+                    if opt_fromIndex < 0 else
+                    opt_fromIndex))
+    
+    if isString(arr):
+      # Array.prototype.indexOf uses === so only strings should be found.
+      if not isString(obj) or len(obj) != 1:
+        return -1
+      return arr.indexOf(obj, fromIndex)
+    
+    for i in range(fromIndex, len(arr)):
+      if (i in arr) and (arr[i] == obj):
+        return i
+    
+    return -1
+
 
